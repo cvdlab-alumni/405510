6c06e2e731c144694d8b84f0f25ac59a2bdcc535
goggio <goggio88@hotmail.it>
2013-06-08 20:39:46 +0200

    reupload

--- 2013-06-07/javascript/exercise6.js
+++ 2013-06-07/javascript/exercise6.js
@@ -2,15 +2,37 @@
  * @author goggio
  */
 //exercise6
-
 //vertici del modello
-v = [[][][][]]
+//v = [[][][][]]
 
 //matrice compatta delle facce 2d
-fv = [[][][][][][]]
+//fv = [[][][][][][]]
 
 
-function lar_to_obj(v,fv){
+function lar_to_obj(v,fv) {
+	
+	var p ="# Vertici : ";
+	p+="\n";
+	
+	for(var i=0;i<v.length;i++){
+		if(v[i].length===2){
+			p+="v : "+ v[i][0] + " "+v[i][1]+" 0 \n"; 
+		}
+		else{
+			 p+= v[i][0] + " " +v[i][1]+ " "+v[i][2]+"\n"; 
+		}
+	}
+	p+="\n";
+	p+=" # 2d matrix faces \n";
+	
+	for(var i=0;i<fv.length;i++){
+		var l = fv[i].length;
+		p +="f : "; 
+		for(j=0;j<l;j++){
+			p +=fv[i][j] + " "; }
+			p+="\n";
+		}
 
-return 
+	return p;
 }
+
